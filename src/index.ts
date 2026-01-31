b53daa1f71a329e5fea1f0a4b2ab6da21f136a99
maker2413 <maker2413@gmail.com>
2026-01-30 19:00:57 -0600

    adding express middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 
 import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponse } from "./api/middleware.js";
 
 const app = express();
 const PORT = 8080;
+
+app.use(middlewareLogResponse);
 
 app.use("/app", express.static("./src/app"));
 
