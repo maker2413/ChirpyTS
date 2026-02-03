3248133f62573e2c20c52dad3546395b964ed319
maker2413 <maker2413@gmail.com>
2026-02-03 00:24:17 -0600

    create chrips api

--- src/api/chirps.ts
+++ src/api/chirps.ts
@@ -1,25 +1,38 @@
 import type { Request, Response } from "express";
 
 import { respondWithJSON } from "./json.js";
+import { createChirp } from "../db/queries/chirps.js";
 import { BadRequestError } from "./errors.js";
 
-export async function handlerChirpsValidate(req: Request, res: Response) {
+export async function handlerChirpsCreate(req: Request, res: Response) {
   type parameters = {
     body: string;
+    userId: string;
   };
 
   const params: parameters = req.body;
 
+  const cleaned = validateChirp(params.body);
+  const chirp = await createChirp({ body: cleaned, userId: params.userId });
+
+  respondWithJSON(res, 201, chirp);
+}
+
+function validateChirp(body: string) {
   const maxChirpLength = 140;
-  if (params.body.length > maxChirpLength) {
+  if (body.length > maxChirpLength) {
     throw new BadRequestError(
       `Chirp is too long. Max length is ${maxChirpLength}`,
     );
   }
 
-  const words = params.body.split(" ");
-
   const badWords = ["kerfuffle", "sharbert", "fornax"];
+  return getCleanedBody(body, badWords);
+}
+
+function getCleanedBody(body: string, badWords: string[]) {
+  const words = body.split(" ");
+
   for (let i = 0; i < words.length; i++) {
     const word = words[i];
     const loweredWord = word.toLowerCase();
@@ -29,8 +42,5 @@ export async function handlerChirpsValidate(req: Request, res: Response) {
   }
 
   const cleaned = words.join(" ");
-
-  respondWithJSON(res, 200, {
-    cleanedBody: cleaned,
-  });
+  return cleaned;
 }
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import {
   middlewareLogResponse,
   middlewareMetricsInc,
 } from "./api/middleware.js";
-import { handlerChirpsValidate } from "./api/chirps.js";
+import { handlerChirpsCreate } from "./api/chirps.js";
 import { handlerUsersCreate } from "./api/users.js";
 import { config } from "./config.js";
 
@@ -35,8 +35,8 @@ app.post("/admin/reset", (req, res, next) => {
   Promise.resolve(handlerReset(req, res)).catch(next);
 });
 
-app.post("/api/validate_chirp", (req, res, next) => {
-  Promise.resolve(handlerChirpsValidate(req, res)).catch(next);
+app.post("/api/chirps", (req, res, next) => {
+  Promise.resolve(handlerChirpsCreate(req, res)).catch(next);
 });
 
 app.post("/api/users", (req, res, next) => {
