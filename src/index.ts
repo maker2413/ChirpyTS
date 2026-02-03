33a4f9797983cb57a8943c10e1d4a583e8fd8751
maker2413 <maker2413@gmail.com>
2026-02-03 01:03:55 -0600

    began authentication

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ import {
 } from "./api/chirps.js";
 import { config } from "./config.js";
 import { handlerUsersCreate } from "./api/users.js";
+import { handlerLogin } from "./api/auth.js";
 
 const migrationClient = postgres(config.db.url, { max: 1 });
 await migrate(drizzle(migrationClient), config.db.migrationConfig);
@@ -37,6 +38,10 @@ app.get("/admin/metrics", (req, res, next) => {
 });
 app.post("/admin/reset", (req, res, next) => {
   Promise.resolve(handlerReset(req, res)).catch(next);
+});
+
+app.post("/api/login", (req, res, next) => {
+  Promise.resolve(handlerLogin(req, res)).catch(next);
 });
 
 app.post("/api/users", (req, res, next) => {
