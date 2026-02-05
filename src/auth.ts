99d3dbb7c3722e2d9ee5ac96430b0516cc2aa682
maker2413 <maker2413@gmail.com>
2026-02-05 01:20:37 -0600

    refresh tokens

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import argon2 from "argon2";
 import jwt from "jsonwebtoken";
+import crypto from "crypto";
 import type { JwtPayload } from "jsonwebtoken";
 
 import { BadRequestError, UserNotAuthenticatedError } from "./api/errors.js";
@@ -73,4 +74,8 @@ export function extractBearerToken(header: string) {
     throw new BadRequestError("Malformed authorization header");
   }
   return splitAuth[1];
+}
+
+export function makeRefreshToken() {
+  return crypto.randomBytes(32).toString("hex");
 }
